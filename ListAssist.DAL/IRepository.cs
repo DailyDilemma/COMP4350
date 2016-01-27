91d7f22291c59d2bac754675eb35206ef5b36100
Josh Westlake <jwestlake@zerocomplexity.ca>
2016-01-27 01:07:07 -0600

    Created a Data Access Layer with repositories and moved the entity framework model into it.

---
+++ ListAssist.DAL/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAssist.DAL
+{
+    public interface IRepository<TEntity> where TEntity: class
+    {
+        TEntity Get(int id);
+        IEnumerable<TEntity> GetAll();
+
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+
+    }
+}
--- ListAssist/App_Start/IdentityConfig.cs
+++ ListAssist/App_Start/IdentityConfig.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
+//using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;

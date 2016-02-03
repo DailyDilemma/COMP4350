86adddd0006343c24279f00ff0e45c37145646f4
Woods0 <markc_63_@hotmail.com>
2016-02-02 19:33:10 -0600

    Initial set up of web api with swagger integration.

--- ListAssist.WebAPI/Global.asax.cs
+++ ListAssist.WebAPI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListAssist.WebAPI.MappingProfile;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,7 @@ namespace ListAssist.WebAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfiguration.Configure();
         }
     }
 }

e4d10d1cb84dce85a9cf6fb0877a6ac4551e35c2
Matt <evolmatt@gmail.com>
2016-02-22 16:20:51 -0600

    Added Selenium to test the front end.  Uses Firefox.

---
+++ ListAssist.Tests/SeleniumUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace ListAssist.Tests
+{
+    [TestClass]
+    public class SeleniumUnitTest
+    {
+        static IWebDriver driverFF;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driverFF = new FirefoxDriver();
+
+        }
+
+        [TestMethod]
+        public void TestOpenApp()
+        {
+            driverFF.Navigate().GoToUrl("http://localhost:2850");
+            
+        }
+
+        [AssemblyCleanup]
+        public static void TearDown()
+        {
+            driverFF.Quit();
+        }
+    }
+}

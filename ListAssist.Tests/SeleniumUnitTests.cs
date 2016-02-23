d9d06ed6dd1a7dd0d96605620ae2a0810d47fa76
Matt <evolmatt@gmail.com>
2016-02-23 00:10:30 -0600

    Add simple selenium test to create empty list

--- ListAssist.Tests/SeleniumUnitTests.cs
+++ ListAssist.Tests/SeleniumUnitTests.cs
@@ -14,24 +14,44 @@ namespace ListAssist.Tests
         public static void SetUp(TestContext context)
         {
             driverFF = new FirefoxDriver();
-
+            driverFF.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
         }
 
-        /* Scenario
+        /* Scenario - Open app.
         1. Open app in browser
-        2. Maximize app
         */
         [TestMethod]
         public void TestOpenApp()
         {
             driverFF.Navigate().GoToUrl("http://localhost:2850");
-            driverFF.Manage().Window.Maximize();
+        }
+
+        /* Scenario - Create new empty list.
+        1. Select Create
+        2. Enter name for new list
+        3. Click create button
+        4. Click save button - doesn't auto redirect to home
+        5. Select Home
+        */
+        [TestMethod]
+        public void TestCreateNewEmptyList()
+        {
+            driverFF.FindElement(By.LinkText("Create")).Click();
+
+            IWebElement element = driverFF.FindElement(By.Name("Name"));
+            element.SendKeys("Empty List");
+            element.Submit();
+
+            element = driverFF.FindElement(By.Name("Name"));
+            element.Submit();
+
+            driverFF.FindElement(By.LinkText("Home")).Click();
         }
 
         [AssemblyCleanup]
         public static void TearDown()
         {
-            driverFF.Quit();
+            //driverFF.Quit();
         }
     }
 }

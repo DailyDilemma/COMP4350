8fdc3b0b2380f48f6637c53ab81a979765049a00
Woods0 <markc_63_@hotmail.com>
2016-02-23 22:25:13 -0600

    Duplicates no longer allowed. Started work making adding a duplicate item that has been checked off reset the checked flag. Added unit tests for webapi. Changed main project so that it doesn't directly reference the database from the controllers

--- ListAssist.Data.Tests/IntegrationTests.cs
+++ ListAssist.Data.Tests/IntegrationTests.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using ListAssist.Data.Models;
+﻿using System.Linq;
 using System.Collections.Generic;
 using System.Data.Entity;
+
+using ListAssist.Data.Models;
+using ListAssist.Data.Queries;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ListAssist.Data.Tests
@@ -14,12 +17,12 @@ namespace ListAssist.Data.Tests
         [TestInitialize]
         public void TestInitialize()
         {
-            LAList testList1 = new LAList() { ID = 30, Name = "Test List 1" };
-            LAList testList2 = new LAList() { ID = 40, Name = "Test List 2" };
+            LAList testList1 = new LAList() { Name = "Test List 1" };
+            LAList testList2 = new LAList() { Name = "Test List 2" };
 
-            LAListItem testInsertItem1 = new LAListItem() { ID = 12, Description = "Test Insert 1", Done = false };
-            LAListItem testInsertItem2 = new LAListItem() { ID = 13, Description = "Test Insert 2", Done = false };
-            LAListItem testInsertItem3 = new LAListItem() { ID = 14, Description = "Test Insert 3", Done = false };
+            LAListItem testInsertItem1 = new LAListItem() { Description = "Test Insert 1", Done = false };
+            LAListItem testInsertItem2 = new LAListItem() { Description = "Test Insert 2", Done = false };
+            LAListItem testInsertItem3 = new LAListItem() { Description = "Test Insert 3", Done = false };
 
             Assert.IsNotNull(testList1, "Test list 1 is null.");
             Assert.IsNotNull(testList2, "Test list 2 is null.");
@@ -49,6 +52,7 @@ namespace ListAssist.Data.Tests
             foreach (LAList newList in lists)
             {
                 this.db.LALists.Add(newList);
+
                 foreach (LAListItem newListItem in newList.LAListItems)
                 {
                     this.db.LAListItems.Add(newListItem);
@@ -56,8 +60,10 @@ namespace ListAssist.Data.Tests
             }
 
             this.db.SaveChanges();
-
-            Assert.IsNotNull(this.db.LALists.Find(30), "List with ID = 30 not found.");
+            
+            Assert.IsNotNull(this.db.LALists.Where(e => e.Name.Equals("Test List 1")).FirstOrDefault(), "Test List 1 not found.");
+            Assert.IsNotNull(this.db.LALists.Where(e => e.Name.Equals("Test List 2")).FirstOrDefault(), "Test List 2 not found.");
+            Assert.IsNull(this.db.LALists.Where(e => e.Name.Equals("Test List 3")).FirstOrDefault(), "Test List 3 found.");
         }
 
         [TestMethod]
@@ -75,52 +81,321 @@ namespace ListAssist.Data.Tests
         }
 
         [TestMethod]
-        public void TestAddListToDB()
+        public void TestGetAllLists()
         {
+            var lists = ListQueries.GetLists();
+
+            Assert.IsNotNull(lists);
+            Assert.AreEqual(4, lists.Count);
+        }
+
+        [TestMethod]
+        public void TestGetSingleList()
+        {
+            var list = ListQueries.GetList(3);
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(3, list.ID);
+            Assert.AreEqual(true, list.Name.Equals("Test List 1"));
+            Assert.AreEqual(2, list.LAListItems.Count);
+        }
+
+        [TestMethod]
+        public void TestGetNonExistantList()
+        {
+            var list = ListQueries.GetList(99);
+
+            Assert.AreEqual(null, list);
+        }
+
+        [TestMethod]
+        public void TestGetListBadListID()
+        {
+            var list = ListQueries.GetList(-99);
+
+            Assert.AreEqual(null, list);
+        }
+
+        [TestMethod]
+        public void TestGetListZeroListID()
+        {
+            var list = ListQueries.GetList(0);
+
+            Assert.AreEqual(null, list);
+        }
+
+        [TestMethod]
+        public void TestAddListToDBNoItems()
+        {
+            var listName = "Test List 4";
+
             var lists = new List<LAList>
             {
-                new LAList { ID = 60, Name = "Test List 4" }
+                new LAList { Name = listName }
             };
                 
             // add the lists to the database along with their associated list items
             foreach ( LAList newList in lists ){
-                this.db.LALists.Add(newList);
+
+                ListQueries.AddList(newList);
+
                 foreach (LAListItem newListItem in newList.LAListItems)
                 {
-                    this.db.LAListItems.Add(newListItem);
+                    ListQueries.AddItemToList(newListItem);
                 }
             }
+
             this.db.SaveChanges();
-            Assert.AreEqual<string>(this.db.LALists.Find(60).Name, "Test List 4", "Test List 4 not found.");
-            Assert.AreEqual<int>(this.db.LALists.Find(60).LAListItems.Count, 0, "Number of list items is not 0.");
+
+            Assert.IsNotNull(db.LALists.Where(e => e.Name.Equals(listName)).FirstOrDefault());
+            Assert.AreEqual(db.LAListItems.Where(e => e.LAList.Name.Equals(listName)).ToList().Count, 0, "Number of list items is not 0");
         }
 
         [TestMethod]
-        public void TestRemove()
+        public void TestAddListWithItems()
         {
-            Assert.AreEqual<int>(2, this.db.LALists.Find(30).LAListItems.Count, "Test list 1 does not contain 2 list items.");
-            this.db.LALists.Find(30).LAListItems.Remove(new LAListItem() { ID = 12, Description = "Test Insert 1", Done = false });
-            Assert.AreEqual<int>(2, this.db.LALists.Find(30).LAListItems.Count, "Test list still contains 3 items.");
+            var newList = new LAList() { Name = "List with items" };
+            newList.LAListItems.Add(new LAListItem() { Description = "Test Item 1", Done = true });
+            newList.LAListItems.Add(new LAListItem() { Description = "Test Item 2", Done = false });
 
-            Assert.IsNotNull(db.LALists.Find(30), "List 3 not found.");
-            this.db.LALists.Remove(this.db.LALists.Find(30));
-            this.db.SaveChanges();
-            Assert.IsNull(this.db.LALists.Find(30), "List 3 not removed.");
+            var result = ListQueries.AddList(newList);
+
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(5, db.LALists.ToList().Count);
+
+            newList = db.LALists.Where(e => e.Name.Equals("List with items")).FirstOrDefault();
+
+            Assert.IsNotNull(newList);
+            Assert.AreEqual(2, newList.LAListItems.Count);
         }
 
         [TestMethod]
-        public void TestUpdate()
+        public void AddDuplicateList()
         {
-            Assert.AreEqual<int>(1, this.db.LALists.Find(40).LAListItems.Count, "Test list 2 does not contain 1 list item.");
-            IEnumerator<LAListItem> itemList1 = this.db.LALists.Find(40).LAListItems.GetEnumerator();
-            Assert.IsNotNull(itemList1.MoveNext(), "Item not found in test list 2.");
-            itemList1.Current.Description = "Update Test";
-            this.db.SaveChanges();
+            var newList = new LAList() { Name = "Test List 1" };
+            var result = ListQueries.AddList(newList);
 
-            Assert.AreEqual<int>(1, this.db.LALists.Find(40).LAListItems.Count, "Test list 2 does not contain 1 list item.");
-            IEnumerator<LAListItem> itemList2 = this.db.LALists.Find(40).LAListItems.GetEnumerator();
-            Assert.IsNotNull(itemList2.MoveNext(), "Item not found in test list 2.");
-            Assert.AreEqual<string>("Update Test", itemList2.Current.Description);
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(4, db.LALists.ToList().Count);
+        }
+
+        [TestMethod]
+        public void AddListWithNullName()
+        {
+            var newList = new LAList();
+            var result = ListQueries.AddList(newList);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(4, db.LALists.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveList()
+        {
+            var firstList = db.LALists.Where(e => e.Name.Equals("Test List 1")).FirstOrDefault();
+
+            Assert.IsNotNull(firstList, "List 3 not found.");
+
+            var result = ListQueries.RemoveList(firstList.ID);
+
+            Assert.AreEqual(true, result);
+            Assert.IsNull(db.LALists.Where(e => e.Name.Equals("Test List 1")).FirstOrDefault(), "List 3 not removed.");
+        }
+
+        [TestMethod]
+        public void TestRemoveNonExistingList()
+        {
+            var result = ListQueries.RemoveList(99);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(4, db.LALists.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveBadListID()
+        {
+            var result = ListQueries.RemoveList(-11);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(4, db.LALists.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveZeroListID()
+        {
+            var result = ListQueries.RemoveList(0);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(4, db.LALists.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestAddListItem()
+        {
+            var newItem = new LAListItem()
+            {
+                Description = "Test Item",
+                Done = true,
+                ListID = 3
+            };
+
+            var result = ListQueries.AddItemToList(newItem);
+
+            Assert.AreEqual(true, result);
+            Assert.IsNotNull(db.LAListItems.Where(e => e.Description.Equals("Test Item")).FirstOrDefault());
+            Assert.AreEqual(3, db.LALists.Find(3).LAListItems.Count);
+        }
+
+        [TestMethod]
+        public void TestAddDuplicateItem()
+        {
+            var newItem = new LAListItem()
+            {
+                Description = "Test Insert 1",
+                ListID = 3
+            };
+
+            var result = ListQueries.AddItemToList(newItem);
+
+            Assert.AreEqual(true, result);
+            Assert.IsNotNull(db.LAListItems.Where(e => e.Description.Equals("Test Insert 1")).FirstOrDefault());
+            Assert.AreEqual(2, db.LALists.Find(3).LAListItems.Count);
+
+            var item = db.LAListItems.Where(e => e.Description.Equals("Test Insert 1")).FirstOrDefault();
+
+            Assert.IsNotNull(item);
+        }
+
+        //[TestMethod]
+        //public void TestResetDoneItem()
+        //{
+        //    var newItem = new LAListItem()
+        //    {
+        //        Description = "Test Insert 0",
+        //        ListID = 3,
+        //        Done = true
+        //    };
+
+        //    var result = ListQueries.AddItemToList(newItem);
+
+        //    Assert.AreEqual(true, result);
+        //    Assert.IsNotNull(db.LAListItems.Where(e => e.Description.Equals("Test Insert 0")).FirstOrDefault());
+        //    Assert.AreEqual(3, db.LALists.Find(3).LAListItems.Count);
+
+        //    result = ListQueries.AddItemToList(newItem);
+
+        //    Assert.AreEqual(true, result);
+        //    Assert.IsNotNull(db.LAListItems.Where(e => e.Description.Equals("Test Insert 0")).FirstOrDefault());
+        //    Assert.AreEqual(3, db.LALists.Find(3).LAListItems.Count);
+
+        //    var item = db.LAListItems.Where(e => e.Description.Equals("Test Insert 0")).FirstOrDefault();
+
+        //    Assert.IsNotNull(item);
+        //    Assert.AreEqual(false, item.Done);
+        //}
+
+        [TestMethod]
+        public void TestAddNullItem()
+        {
+            var result = ListQueries.AddItemToList(null);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(2, db.LALists.Find(3).LAListItems.Count);
+        }
+
+        [TestMethod]
+        public void TestAddItemToNoList()
+        {
+            var newItem = new LAListItem() { Description = "Test Insert 0", Done = false };
+            var itemCount = db.LAListItems.ToList().Count;
+            var result = ListQueries.AddItemToList(newItem);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual(itemCount, db.LAListItems.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveListItem()
+        {
+            var item = db.LAListItems.Find(8);
+            var itemCount = db.LAListItems.ToList().Count;
+
+            Assert.IsNotNull(item);
+
+            var result = ListQueries.DeleteItemFromList(item.ID);
+
+            Assert.AreEqual(3, result);
+            Assert.AreEqual(true, itemCount > db.LAListItems.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveNonExistantItem()
+        {
+            var itemCount = db.LAListItems.ToList().Count;
+
+            var result = ListQueries.DeleteItemFromList(99);
+
+            Assert.AreEqual(-1, result);
+            Assert.AreEqual(itemCount, db.LAListItems.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveItemBadId()
+        {
+            var itemCount = db.LAListItems.ToList().Count;
+
+            var result = ListQueries.DeleteItemFromList(-99);
+
+            Assert.AreEqual(-1, result);
+            Assert.AreEqual(itemCount, db.LAListItems.ToList().Count);
+        }
+
+        [TestMethod]
+        public void TestUpdateListName()
+        {
+            var list = db.LALists.Find(3);
+
+            Assert.IsNotNull(list);
+            
+            list.Name = "Updated Name";
+
+            var result = ListQueries.UpdateList(list);
+
+            Assert.AreEqual(true, result);
+            Assert.AreEqual("Updated Name", db.LALists.Find(3).Name);
+            Assert.AreEqual(2, db.LALists.Find(3).LAListItems.Count);
+        }
+
+        [TestMethod]
+        public void TestUpdateListNameNull()
+        {
+            var list = db.LALists.Find(3);
+
+            Assert.IsNotNull(list);
+
+            list.Name = null;
+
+            var result = ListQueries.UpdateList(list);
+
+            Assert.AreEqual(false, result);
+           // Assert.AreEqual("Test List 1", db.LALists.Find(3).Name);
+            Assert.AreEqual(2, db.LALists.Find(3).LAListItems.Count);
+        }
+
+        [TestMethod]
+        public void TestUpdateListAddNullItems()
+        {
+            var list = db.LALists.Find(3);
+
+            Assert.IsNotNull(list);
+
+            list.LAListItems = null;
+
+            var result = ListQueries.UpdateList(list);
+
+            Assert.AreEqual(false, result);
+            Assert.AreEqual("Test List 1", db.LALists.Find(3).Name);
+            //Assert.AreEqual(2, db.LALists.Find(3).LAListItems.Count);
         }
     }
 }
---
+++ ListAssist.Data/Queries/ListQueries.cs
@@ -0,0 +1,130 @@
+﻿using ListAssist.Data.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ListAssist.Data.Queries
+{
+    public static class ListQueries
+    {
+        private static ListAssistContext db = new ListAssistContext();
+
+        public static List<LAList> GetLists()
+        {
+            return db.LALists.OrderBy(e => e.Name).ToList();
+        }
+
+        public static LAList GetList(int id)
+        {
+            return db.LALists.Find(id);
+        }
+
+        public static bool AddList(LAList list)
+        {
+            bool success = false;
+
+            if (list.Name != null)
+            {
+                var duplicate = db.LALists.Where(e => e.Name.Equals(list.Name)).FirstOrDefault();
+
+                if (duplicate == null)
+                {
+                    db.LALists.Add(list);
+                    db.SaveChanges();
+
+                    success = true;
+                }
+            }
+
+            return success;
+        }
+
+        public static bool RemoveList(int id)
+        {
+            bool success = false;
+            var list = db.LALists.Find(id);
+
+            if(list != null)
+            {
+                db.LALists.Remove(list);
+                db.SaveChanges();
+
+                success = true;
+            }
+
+            return success;
+        }
+
+        public static bool UpdateList(LAList list)
+        {
+            bool success = false;
+
+            if (list != null)
+            {
+                if (list.Name != null && list.LAListItems != null)
+                {
+                    db.Entry(list).State = EntityState.Modified;
+                    db.LALists.Attach(list);
+
+                    foreach (var listItem in list.LAListItems)
+                    {
+                        db.Entry(listItem).State = EntityState.Modified;
+                    }
+
+                    db.SaveChanges();
+
+                    success = true;
+                }
+            }
+
+            return success;
+        }
+
+        public static bool AddItemToList(LAListItem item)
+        {
+            var success = false;
+
+            if (item != null)
+            {
+                var list = db.LALists.Find(item.ListID);
+
+                if (list != null)
+                {
+                    var duplicate = db.LAListItems.Where(e => (e.ListID == item.ListID) && (e.Description.Equals(item.Description))).FirstOrDefault();
+
+                    if (duplicate == null)
+                    {
+                        db.LAListItems.Add(item);
+                    }
+                    //else if (duplicate.Done)
+                    //{
+                    //    duplicate.Done = false;
+                    //    db.Entry(duplicate).State = EntityState.Modified;
+                    //}
+
+                    db.SaveChanges();
+
+                    success = true;
+                }
+            }
+
+            return success;
+        }
+
+        public static int DeleteItemFromList(int id)
+        {
+            int listId = -1;
+            var item = db.LAListItems.Find(id);
+
+            if (item != null)
+            {
+                listId = item.ListID;
+
+                db.LAListItems.Remove(item);
+                db.SaveChanges();
+            }
+
+            return listId;
+        }
+    }
+}

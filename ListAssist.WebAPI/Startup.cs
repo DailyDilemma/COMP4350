bf912428df2748d1c3fb80b2279d53b3c8cf776a
Airistotal <danieltos.bouchard@gmail.com>
2016-02-25 00:23:30 -0600

    Removed unnecessary files, ignored xml format warning 1591

--- ListAssist.WebAPI/Startup.cs
+++ ListAssist.WebAPI/Startup.cs
@@ -12,7 +12,7 @@ namespace ListAssist.WebAPI
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            //ConfigureAuth(app);
         }
     }
 }
